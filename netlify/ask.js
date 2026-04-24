59592dcf76445e3f035bbeea017a1093c61f120a
yashrajjadhav2010 <yashrajart1999@gmail.com>
2026-04-24 14:33:27 +0530

    Create ask.js

---
+++ netlify/ask.js
@@ -0,0 +1,34 @@
+export async function handler(event) {
+  try {
+    const { prompt } = JSON.parse(event.body);
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [{ text: prompt }]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Error" })
+    };
+  }
+}
